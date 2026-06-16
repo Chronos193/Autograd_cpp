ef1d3347cecaf775b068c8e27aaafce279dbdb69
Shubham Kumar Patel <shubhkrpatel@gmail.com>
2026-06-17 02:40:35 +0530

    Modified some comments

--- include/perceptron.h
+++ include/perceptron.h
@@ -10,7 +10,7 @@ class Perceptron
 
     public:
         Perceptron(int n) : bias(0.0f) {
-            // ============= Random Initialization of weights =====================
+            // Random initialization
             std::random_device rd;
             std::mt19937 gen(rd());
             std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
